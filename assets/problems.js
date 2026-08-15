6a2e9db7061ec1255beb64eb1c5b8a604f48b409
yvaishalirao <yellurivaishalirao@gmail.com>
2026-08-15 17:32:00 +0530

    738 LC

--- assets/problems.js
+++ assets/problems.js
@@ -53,4 +53,5 @@ const PROBLEMS = [
   { num: '507', title: "Perfect Number", file: '0507-perfect-number.html' },
   { num: '628', title: "Maximum Product of Three Numbers", file: '0628-maximum-product-of-three-numbers.html' },
   { num: '958', title: "Check Completeness of a Binary Tree", file: '0958-check-completeness-of-a-binary-tree.html' },
+  { num: '738', title: "Monotone Increasing Digits", file: '0738-monotone-increasing-digits.html' },
 ];
